028894f00451e2569f3609a39692416958a24e23
shahab shafie <80480584+Shhb-Coder-1999@users.noreply.github.com>
2021-04-09 23:35:59 +0430

    Some edit for deployment

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
  git push -f git@github.com:
-okabe-rintaru/heap-Sorts-Comparison/src/app.git master:gh-pages
+okabe-rintaru/heap-Sorts-Comparison/src/App.git master:gh-pages
 
 cd -
